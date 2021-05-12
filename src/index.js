c4834874f8b3a973bb7d35afec22dca14e86409c
AlexMartin998 <alextravc.342@gmail.com>
2021-05-12 08:22:33 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,111 +1,5 @@
 'use strict';
 import './styles.css';
-/* 
-const message = document.querySelector('.message'),
-  number = document.querySelector('.number'),
-  score = document.querySelector('.score'),
-  guess = document.querySelector('.guess');
-
-console.log(message.textContent);
-
-message.textContent = 'Correct Number!!';
-number.textContent = 13;
-score.textContent = 10;
-guess.value = 23;
-console.log(guess.value); 
-*/
-
-// -------------------------------------------------------------------
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-/** Events:Para poder escuchar eventos, primero debemos seleccionar el elemento donde debe ocurrir el evento.
- * ES el motor de JS el que llamara a la f(x) / callback cuando se dispare el evento que esta escuchando.
- */
-/* 
-const message = document.querySelector('.message'),
-  number = document.querySelector('.number'),
-  score = document.querySelector('.score'),
-  check = document.querySelector('.check'),
-  bodyWin = document.querySelector('body'),
-  input = document.getElementById('input'),
-  highscoreL = document.querySelector('.highscore'),
-  again = document.querySelector('.again'),
-  guess = document.querySelector('.guess');
-
-let secretNumber = Math.trunc(Math.random() * 20) + 1,
-  scoreJs = 20,
-  highscore = 0;
-
-score.textContent = scoreJs;
-console.log(secretNumber);
-
-check.onclick = () => {
-  const guessValue = Number(guess.value);
-
-  // When there is no input
-  if (!guessValue) {
-    message.textContent = '⛔️ No number!!';
-
-    //When player wins
-  } else if (guessValue === secretNumber) {
-    message.textContent = '🎉 Correct Number!';
-    bodyWin.classList.add('body-win');
-    number.textContent = secretNumber;
-
-    number.style.width = '30rem';
-
-    if (scoreJs > highscore) {
-      highscore = scoreJs;
-      highscoreL.textContent = highscore;
-    }
-
-    // When guess is too high
-  } else if (guessValue > secretNumber) {
-    if (scoreJs > 1) {
-      scoreJs--;
-      message.textContent = '📈 Too high!';
-      score.textContent = scoreJs;
-    } else {
-      message.textContent = '💥 You lost the game!';
-      score.textContent = 0;
-    }
-
-    // When guess is too low
-  } else if (guessValue < secretNumber) {
-    if (scoreJs > 0) {
-      scoreJs--;
-      message.textContent = '📉 Too low!';
-      score.textContent = scoreJs;
-    } else {
-      message.textContent = '💥 You lost the game!';
-      score.textContent = 0;
-    }
-  }
-};
-
-input.addEventListener('keyup', e => {
-  if (e.keyCode === 13 && input.value.length >= 0) {
-    check.click();
-  }
-});
-
-again.onclick = () => {
-  scoreJs = 20;
-  score.textContent = scoreJs;
-  bodyWin.classList.remove('body-win');
-  guess.value = '';
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  number.textContent = '?';
-  number.style.width = '15rem';
-  console.log(secretNumber);
-};
-*/
-
-// -------------------------------------------------------------------
-//////////////////////////////////////////////////////////////////////
-/** Refactoring
- *
- */
 
 const message = document.querySelector('.message'),
   number = document.querySelector('.number'),
@@ -119,7 +13,7 @@ const message = document.querySelector('.message'),
   guess = document.querySelector('.guess');
 
 let secretNumber = 0,
-  scoreJs = 5,
+  scoreJs = 20,
   highscore = 0;
 
 score.textContent = scoreJs;
@@ -167,7 +61,6 @@ const lostGame = () => {
 };
 
 createSecretNum();
-console.log(secretNumber);
 
 check.onclick = () => {
   const guessValue = Number(guess.value);
@@ -184,19 +77,6 @@ check.onclick = () => {
       lostGame();
     }
   }
-
-  // // // Teniendo 2 f(x) iguales, solo cambia mensaje: high/low:
-  //   else {
-  //     guessValue === secretNumber
-  //       ? playerWins()
-  //       : guessValue > secretNumber && scoreJs > 1
-  //       ? tooHigh()
-  //       : guessValue < secretNumber && scoreJs > 1
-  //       ? tooLow()
-  //       : lostGame();
-  //   }
-
-  //   console.log(scoreJs);
 };
 
 input.addEventListener('keyup', e => {
@@ -206,7 +86,7 @@ input.addEventListener('keyup', e => {
 });
 
 again.onclick = () => {
-  scoreJs = 5;
+  scoreJs = 20;
   printScore(scoreJs);
   bodyWin.classList.remove('body-win');
   guess.value = '';
@@ -214,7 +94,6 @@ again.onclick = () => {
 
   number.textContent = '?';
   number.style.width = '15rem';
-  console.log(secretNumber);
 
   btnCheck.disabled = false;
 };
